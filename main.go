fb9e30598227fa29c1453d72771b65f6fa9eb0c6
echo <113820183+echo-66@users.noreply.github.com>
2023-10-27 02:48:51 +0000

    add 1

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
-import (
-  "fmt"
-)
+
+import "github.com/gin-gonic/gin"
 
 func main() {
-  fmt.Printf("test")
-  }
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	router.Run()
+}
